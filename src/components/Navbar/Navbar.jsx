042bd0c6c19cd2e4e58a8653a18f4f751403ef98
oscarhillz <oscarhillz@hotmail.com>
2024-04-26 16:19:42 -0500

    navbar drawer color y links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,6 +5,10 @@ import HouseIcon from '@mui/icons-material/House';
 import FaceIcon from '@mui/icons-material/Face';
 import AccountTreeIcon from '@mui/icons-material/AccountTree';
 import MenuIcon from '@mui/icons-material/Menu';
+import XIcon from '@mui/icons-material/X';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import GithubIcon from '@mui/icons-material/GitHub';
 
 import { Link } from 'react-router-dom'
 import { Drawer } from '@mui/material'
@@ -14,6 +18,10 @@ import { useState } from 'react'
 export default function Navbar() {
   const icons = [<HouseIcon />, <FaceIcon />, <AccountTreeIcon />]
   const texts = ['Home', 'About', 'Projects']
+
+  const DrawerIcons = [<XIcon />, <LinkedInIcon />, <InstagramIcon />, <GithubIcon />];
+  const DrawerIconsLinks = ['https://twitter.com/oscarhillz' ,'https://www.linkedin.com/in/oscarhillz', 'https://www.instagram.com/oscarhillz/', 'https://www.github.com/oscarhillz/']
+  const quickLinks = ['Home', 'About', 'Projects', 'Services', 'Contact'];
 
   const [isDrawerOpen, setIsDrawerOpen] = useState(false)
 
@@ -41,7 +49,28 @@ export default function Navbar() {
     </div>
 
     <Drawer anchor='left' open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
-      Hola como estas?
+      <div className="Drawer">
+        <div className="DrawerHeader">
+          <div className="DrawerHeaderIMG"></div>
+          <h2>Oscar Hill</h2>
+        </div>
+
+        <div className="DrawerLinks">
+          {quickLinks.map((link, index) => (
+            <Link key={index} to={`/CV/${link}`}>
+                {link}
+            </Link>
+          ))}
+        </div>
+
+        <div className="DrawerFooter">
+          {DrawerIcons.map((icon, index) => (
+            <a key={index} target="_blank" href={DrawerIconsLinks[index]}>
+              {icon}
+            </a>
+          ))}
+        </div>
+      </div>
 
 
     </Drawer>
