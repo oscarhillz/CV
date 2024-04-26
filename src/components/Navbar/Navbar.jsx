4e4ca64bc862454e4589dcd1a5cbfeba0f0f9dd1
oscarhillz <oscarhillz@hotmail.com>
2024-04-26 16:38:13 -0500

    navbar con iconos

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -22,58 +22,59 @@ export default function Navbar() {
   const DrawerIcons = [<XIcon />, <LinkedInIcon />, <InstagramIcon />, <GithubIcon />];
   const DrawerIconsLinks = ['https://twitter.com/oscarhillz' ,'https://www.linkedin.com/in/oscarhillz', 'https://www.instagram.com/oscarhillz/', 'https://www.github.com/oscarhillz/']
   const quickLinks = ['Home', 'About', 'Projects', 'Services', 'Contact'];
+  const quickLinksIcons = [<HouseIcon />, <FaceIcon />, <AccountTreeIcon />, <AccountTreeIcon />, <AccountTreeIcon />]
 
   const [isDrawerOpen, setIsDrawerOpen] = useState(false)
 
   return (
     <>
-    <div className="Navbar">
+      <div className="Navbar">
         <div className="nav-menu">
-            <div className="nav-container">
-                {icons.map((icon, index) => (
-                  <Link key={index} to={`/CV/${texts[index]}`}>
-                      <div className="Navbar-icon">
-                          {icon}
-                          {texts[index]}
-                      </div>
-                  </Link>
-                ))}
-
-                <div className="Navbar-icon" onClick={() => setIsDrawerOpen(true)}>
-                  <MenuIcon />
-                  Menu
+          <div className="nav-container">
+            {icons.map((icon, index) => (
+              <Link key={index} to={`/CV/${texts[index]}`}>
+                <div className="Navbar-icon">
+                  {icon}
+                  {texts[index]}
                 </div>
+              </Link>
+            ))}
 
+            <div className="Navbar-icon" onClick={() => setIsDrawerOpen(true)}>
+              <MenuIcon />
+              Menu
             </div>
-        </div>
-    </div>
-
-    <Drawer anchor='left' open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
-      <div className="Drawer">
-        <div className="DrawerHeader">
-          <div className="DrawerHeaderIMG"></div>
-          <h2>Oscar Hill</h2>
-        </div>
-
-        <div className="DrawerLinks">
-          {quickLinks.map((link, index) => (
-            <Link key={index} to={`/CV/${link}`}>
-                {link}
-            </Link>
-          ))}
-        </div>
-
-        <div className="DrawerFooter">
-          {DrawerIcons.map((icon, index) => (
-            <a key={index} target="_blank" href={DrawerIconsLinks[index]}>
-              {icon}
-            </a>
-          ))}
+          </div>
         </div>
       </div>
 
+      <Drawer anchor="left" open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
+        <div className="Drawer">
+          <div className="DrawerHeader">
+            <div className="DrawerHeaderIMG"></div>
+            <h2>Oscar Hill</h2>
+          </div>
 
-    </Drawer>
+          <div className="DrawerLinks">
+            {quickLinks.map((link, index) => (
+              <Link key={index} to={`/CV/${link}`}>
+                <div className="DrawerLink">
+                  {quickLinksIcons[index]}
+                  {link}
+                </div>
+              </Link>
+            ))}
+          </div>
+
+          <div className="DrawerFooter">
+            {DrawerIcons.map((icon, index) => (
+              <a key={index} target="_blank" href={DrawerIconsLinks[index]}>
+                {icon}
+              </a>
+            ))}
+          </div>
+        </div>
+      </Drawer>
     </>
-  )
+  );
 }
