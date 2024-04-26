340b5ef2f824daaf69049a117bb740ce9021d577
oscarhillz <oscarhillz@hotmail.com>
2024-04-26 15:23:18 -0500

    arreglo de navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,31 @@
 
 import './Navbar.css'
-import Navbar_icon from './Navbar_icon/Navbar_icon'
 
 import HouseIcon from '@mui/icons-material/House';
 import FaceIcon from '@mui/icons-material/Face';
 import AccountTreeIcon from '@mui/icons-material/AccountTree';
 import MenuIcon from '@mui/icons-material/Menu';
 
+import { Link } from 'react-router-dom'
+
 export default function Navbar() {
   const icons = [<HouseIcon />, <FaceIcon />, <AccountTreeIcon />, <MenuIcon />]
   const texts = ['Home', 'About', 'Projects', 'Menu']
-
   
   return (
     <div className="Navbar">
         <div className="nav-menu">
             <div className="nav-container">
                 {icons.map((icon, index) => (
-                  <Navbar_icon key={index} icon={icon} text={texts[index]} />
+                  <Link key={index} to={`/CV/${texts[index]}`}>
+                      <div className="Navbar-icon">
+                          {icon}
+                          {texts[index]}
+                      </div>
+                  </Link>
                 ))}
             </div>
         </div>
-
-
     </div>
   )
 }
